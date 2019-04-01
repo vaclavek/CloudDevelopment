01b970ef969117751c73edfd8093d8fb0d861766
Ondřej Václavek <vaclavek@havit.cz>
2019-04-01 13:54:10 +0200

    Add Event Hub

---
+++ Lab-2019-04-03/01_EventHubPublisher/EventHubPublisher/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace EventHubPublisher
+{
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.EventHubs;
+
+    // Source: https://docs.microsoft.com/en-us/azure/event-hubs/event-hubs-dotnet-standard-getstarted-send
+    public class Program
+    {
+        private static EventHubClient eventHubClient;
+        private const string EventHubConnectionString = "<INSERT YOUR CONNECTION STRING>"; // ie Endpoint=sb://XXX.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=XXX
+        private const string EventHubName = "<INSERT YOUR EVENT HUB NAME>"; // ie myeventhub1
+
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
+            // Typically, the connection string should have the entity path in it, but for the sake of this simple scenario
+            // we are using the connection string from the namespace.
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
+            {
+                EntityPath = EventHubName
+            };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            await SendMessagesToEventHub(10);
+
+            await eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+
+        // Creates an event hub client and sends X messages to the event hub.
+        private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        {
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    var message = $"Message {i}";
+                    Console.WriteLine($"Sending message: {message}");
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(10);
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+        }
+    }
+}
---
+++ Lab-2019-04-03/01_EventHubPublisher/README.md
@@ -0,0 +1,32 @@
+# CloudDevelopment
+## Event Hub
+
+*Task: Create an Event Hub on your Azure account and use attached application to publish an event*
+
+1. Find Event Hubs in Azure Portal
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/eh_1_portal.png)
+
+2. Add Event Hub
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/eh_2_add.png)
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/eh_3_add.png)
+
+3. Add new Event Hub entity
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/eh_4_new.png)
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/eh_5_new.png)
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/eh_6_neweh.png)
+
+4. Get connection string of Event Hub
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/eh_7_connectionstring.png)
+
+5. Run application [EventHubPublisher](EventHubPublisher.sln)
+
+6. Check if events have been sent to Event Hub
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/eh_8_events.png)
