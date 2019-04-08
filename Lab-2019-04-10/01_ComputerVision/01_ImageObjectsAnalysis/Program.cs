8638ed23f2ea430a1630178f72c57097196b2a40
Ondřej Václavek <vaclavek@havit.cz>
2019-04-08 14:44:26 +0200

    Add computer vision

---
+++ Lab-2019-04-10/01_ComputerVision/01_ImageObjectsAnalysis/Program.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ImageObjectsAnalysis
+{
+    static class Program
+    {
+        // DOC: https://docs.microsoft.com/cs-cz/azure/cognitive-services/computer-vision/quickstarts/csharp-analyze
+
+        // Replace <Subscription Key> with your valid subscription key.
+        const string subscriptionKey = "34b6829f9c6d4f22a9f1a0c7f603f0de";
+
+        // You must use the same Azure region in your REST API method as you used to
+        // get your subscription keys. For example, if you got your subscription keys
+        // from the West US region, replace "westcentralus" in the URL
+        // below with "westus".
+        //
+        // Free trial subscription keys are generated in the "westus" region.
+        // If you use a free trial subscription key, you shouldn't need to change
+        // this region.
+        const string uriBase = "https://westeurope.api.cognitive.microsoft.com/vision/v2.0/analyze";
+
+        static void Main()
+        {
+            // Get the path and filename to process from the user.
+            Console.WriteLine("Analyze an image:");
+            Console.Write("Enter the path to the image you wish to analyze: ");
+            string imageFilePath = Console.ReadLine();
+
+            if (File.Exists(imageFilePath))
+            {
+                // Call the REST API method.
+                Console.WriteLine("\nWait a moment for the results to appear.\n");
+                MakeAnalysisRequest(imageFilePath).Wait();
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid file path");
+            }
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Gets the analysis of the specified image file by using
+        /// the Computer Vision REST API.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to analyze.</param>
+        static async Task MakeAnalysisRequest(string imageFilePath)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                // Request headers.
+                client.DefaultRequestHeaders.Add(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                // Request parameters. A third optional parameter is "details".
+                // The Analyze Image method returns information about the following
+                // visual features:
+                // Categories:  categorizes image content according to a
+                //              taxonomy defined in documentation.
+                // Description: describes the image content with a complete
+                //              sentence in supported languages.
+                // Color:       determines the accent color, dominant color, 
+                //              and whether an image is black & white.
+                string requestParameters =
+                    "visualFeatures=Categories,Description,Color";
+
+                // Assemble the URI for the REST API method.
+                string uri = uriBase + "?" + requestParameters;
+
+                HttpResponseMessage response;
+
+                // Read the contents of the specified local image
+                // into a byte array.
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+                // Add the byte array as an octet stream to the request body.
+                using (ByteArrayContent content = new ByteArrayContent(byteData))
+                {
+                    // This example uses the "application/octet-stream" content type.
+                    // The other content types you can use are "application/json"
+                    // and "multipart/form-data".
+                    content.Headers.ContentType =
+                        new MediaTypeHeaderValue("application/octet-stream");
+
+                    // Asynchronously call the REST API method.
+                    response = await client.PostAsync(uri, content);
+                }
+
+                // Asynchronously get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Display the JSON response.
+                Console.WriteLine("\nResponse:\n\n{0}\n",
+                    JToken.Parse(contentString).ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            // Open a read-only file stream for the specified file.
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                // Read the file's contents into a byte array.
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+    }
+}
---
+++ Lab-2019-04-10/01_ComputerVision/README.md
@@ -0,0 +1,84 @@
+# CloudDevelopment
+## Cognitive Services - Computer Vision [(docs)](https://azure.microsoft.com/cs-cz/services/cognitive-services/computer-vision/)
+
+*Tasks*
+1. Make/find a photo and make an analysis of content of image.
+1. Make/find a real-world photo with some text on it and try to parse the text.
+1. Make/find a photo and let the cloud detect more informations (age, gender, hair color, emotions)
+
+## Task 1 - Image analysis ([online example](https://azure.microsoft.com/cs-cz/services/cognitive-services/computer-vision/#analyze))
+
+1. Find Cognitive Services in Azure Portal
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/cv_1_portal.png)
+
+2. Add new Computer images processing service
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/cv_2_portal.png)
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/cv_3_portal.png)
+
+3. Find and copy your generated keys
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/cv_4_portal.png)
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/cv_5_keys.png)
+
+4. Use [attached application](https://github.com/vaclavek/CloudDevelopment/tree/master/Lab-2019-04-10/01_ImageObjectsAnalysis), insert your own subscription key and run application
+
+5.  Processed image (use your own)
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/cv_6_image.jpg)
+
+6.  Look at the results:
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/cv_7_result.png)
+
+7.  You can see your request in Azure portal
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/cv_8_graph.png)
+
+## Task 2 - Text analysis on the image ([online example](https://azure.microsoft.com/cs-cz/services/cognitive-services/computer-vision/#text)), [HTML wireframe analysis - video](https://www.youtube.com/watch?v=_oet4GOzcRQ)
+
+1. You can use subscription key from Task 1 (or create new)
+
+2. Use [attached application](https://github.com/vaclavek/CloudDevelopment/tree/master/Lab-2019-04-10/02_OCR), insert your own subscription key and run application
+
+3. Processed image (use your own)
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/ocr_1_image.jpg)
+
+4. Look at the results:
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/ocr_1_result.png)
+
+## Task 3 - Face detection
+
+1. Find Cognitive Services in Azure Portal
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/cv_1_portal.png)
+
+2. Add new Face service to Cognitive Services
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/face_1_portal.png)
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/face_2_portal.png)
+
+3. Find and copy your generated keys (like in tasks before)
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/face_3_portal.png)
+
+4. Use [attached application](https://github.com/vaclavek/CloudDevelopment/tree/master/Lab-2019-04-10/03_FaceDetection), insert your own subscription key and run application
+
+5. Processed images (use your own)
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/face_4_image.jpg)
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/face_5_response.png)
+
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/face_6_image.jpg)
+![](https://raw.githubusercontent.com/vaclavek/CloudDevelopment/master/Images/face_7_response.png)
+
+## Aditional resources
+1. [Face detection](https://docs.microsoft.com/en-US/azure/cognitive-services/face/face-api-how-to-topics/howtodetectfacesinimage)
+2. [Face identification](https://docs.microsoft.com/en-us/azure/cognitive-services/face/face-api-how-to-topics/howtoidentifyfacesinimage)
+3. [Sample applications](https://azure.microsoft.com/en-us/resources/samples/?service=cognitive-services&term=Face&sort=0)
+
